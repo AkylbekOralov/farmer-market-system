0a12a20f4987a52d31d2b77d17031fe3f066c15b
oralovv <oralov26@gmail.com>
2024-10-27 13:51:50 +0500

    Get and Add to DB

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// server.js
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
+const { validate } = require("deep-email-validator");
 
 const app = express();
 const port = 8383;
 
+// Connecting to mysql
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -22,18 +23,35 @@ app.use(cors());
 app.use(express.json());
 
 // Registration endpoint
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
-  const sql = "INSERT INTO users (email, password) VALUES (?, ?)";
-  con.query(sql, [email, password], (err, result) => {
-    if (err) {
-      console.error("Error inserting user data:", err);
-      return res.status(500).send("Registration failed");
+  try {
+    // Validate the email
+    const validationResult = await validate(email);
+
+    if (!validationResult.valid) {
+      return res.status(400).send({
+        status: "error",
+        message: "Email is not valid. Please try again!",
+        reason: validationResult.reason,
+      });
     }
-    res.status(201).send("User registered successfully");
-    console.log(`Added new user ${email} ${password}`);
-  });
+
+    const sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+    con.query(sql, [email, password], (err, result) => {
+      if (err) {
+        console.error("Error inserting user data:", err);
+        return res.status(500).send("Registration failed");
+      }
+      res.status(201).send("User registered successfully");
+      console.log(`Added new user: ${email}`);
+    });
+  } catch (error) {
+    console.error("Error during email validation:", error);
+    res.status(500).send("Email validation failed");
+  }
 });
 
+// Hosting
 app.listen(port, () => console.log(`Server has started on port: ${port}`));
