26af655bff19b2906fad38ef4c9c75cc424e9869
AkkuAku <akku.saniyazbekova@nu.edu.kz>
2024-11-21 21:56:30 +0600

    Update buyerController.js

--- controllers/buyerController.js
+++ controllers/buyerController.js
@@ -1,6 +1,6 @@
-// controllers/buyerController.js
 const { Product, Category } = require("../models");
 
+// Fetch all products with category details
 exports.viewProducts = async (req, res) => {
   try {
     const products = await Product.findAll({
@@ -16,6 +16,20 @@ exports.viewProducts = async (req, res) => {
   }
 };
 
+// Fetch all categories
+exports.viewCategories = async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      attributes: ["id", "name"], // Fetch only ID and name fields
+    });
+    res.status(200).json(categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Place an order (stubbed for now)
 exports.placeOrder = async (req, res) => {
   try {
     const products = await Product.findAll({
